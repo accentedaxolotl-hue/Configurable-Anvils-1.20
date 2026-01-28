37db0d28733d81c3135a7f67ec7e0feabdef9c9b
Tsukamoto <tsukamototheeditor@gmail.com>
2026-01-28 13:02:25 +0500

    Fixed mayPickup() bug

--- src/main/java/net/itwaskairo/configurableanvils/mixin/AnvilMenuMixin.java
+++ src/main/java/net/itwaskairo/configurableanvils/mixin/AnvilMenuMixin.java
@@ -1,13 +1,16 @@
 package net.itwaskairo.configurableanvils.mixin;
 
 import net.itwaskairo.configurableanvils.ConfigurableAnvilsConfig;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.inventory.DataSlot;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = AnvilMenu.class, priority = 1001)
 public class AnvilMenuMixin {
@@ -28,12 +31,6 @@ public class AnvilMenuMixin {
         }
         return 50;
     }
-
-    //@Shadow
-    //private boolean onlyRenaming;
-
-    @Shadow
-    private DataSlot cost;
 
     @ModifyConstant(
             method = "createResult",
@@ -59,24 +56,38 @@ public class AnvilMenuMixin {
         return MaxLength();
     }
 
-    @Inject(method = "mayPickup", at = @At("TAIL"), cancellable = true)
-    private void changedMayPickup(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(
+            method = "mayPickup",
+            at = @At("TAIL"),
+            cancellable = true
+    )
+    private void changedMayPickup(Player player, boolean bool, CallbackInfoReturnable<Boolean> cir) {
         if (ConfigurableAnvilsConfig.SERVER.enableCustomRenamingCost.get()) {
-            cir.setReturnValue(this.cost.get() > -1);
+            cir.setReturnValue (player.getAbilities().instabuild || player.experienceLevel >= this.cost.get() && this.cost.get() > -1);
         }
+        cir.setReturnValue(player.getAbilities().instabuild || player.experienceLevel >= this.cost.get() && this.cost.get() > 0);
     }
 
-    /*
+    @Shadow private DataSlot cost;
+
     @Inject(
             method = "createResult",
-            at = @At("TAIL")
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/inventory/AnvilMenu;broadcastChanges()V"
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private void fixRenameCost(CallbackInfo ci) {
-        if (!ConfigurableAnvilsConfig.SERVER.enableCustomRenamingCost.get()) return;
-
-        if (this.onlyRenaming) {
-            this.cost.set(ConfigurableAnvilsConfig.SERVER.customRenamingCost.get());
+    private void interceptRenameCost(CallbackInfo ci,
+                                     ItemStack itemstack,
+                                     int i,
+                                     int j,
+                                     int k
+    ) {
+        if (k > 0 && i == k) {
+            if (ConfigurableAnvilsConfig.SERVER.enableCustomRenamingCost.get()) {
+                this.cost.set(ConfigurableAnvilsConfig.SERVER.customRenamingCost.get());
+            }
         }
     }
-    */
 }
